98dae83d45127ce6d197e627c90eff99c82c0415
ho-jun97 <hojun9707@naver.com>
2022-10-31 16:46:23 +0900

    test: Register Membership

--- src/test/java/com/example/tdd/MembershipRepositoryTest.java
+++ src/test/java/com/example/tdd/MembershipRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.tdd;
 
+import com.example.tdd.domain.Membership;
 import com.example.tdd.domain.MembershipRepository;
+import com.example.tdd.domain.MembershipType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,5 +21,22 @@ public class MembershipRepositoryTest {
         assertThat(membershipRepository).isNotNull();
     }
 
+    @Test
+    public void 멤버심등록(){
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName(MembershipType.NAVER)
+                .point(10000)
+                .build();
+        // when
 
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo(MembershipType.NAVER);
+        assertThat(result.getPoint()).isEqualTo(10000);
+    }
 }

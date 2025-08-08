5c53bcd9b5e77557890cf15cb68277f7408e5a61
ldr <hojun9707@naver.com>
2025-08-08 16:48:56 +0900

    feat: 예외 처리

---
+++ src/main/java/com/example/tdd/exception/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.example.tdd.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        final List<String> errorList = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        log.warn("Invalid DTO Prameter erros : {}", errorList);
+
+        return this.makeErrorResponseEntity(errorList.toString());
+    }
+
+    private ResponseEntity<Object> makeErrorResponseEntity(final String errorDescription) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), errorDescription));
+    }
+    @ExceptionHandler(MembershipException.class)
+    public ResponseEntity<ErrorResponse> handleRestApiException(final MembershipException exception) {
+        log.warn("MembershipException occur : ", exception);
+        return this.makeErrorResponseEntity(exception.getErrorResult());
+    }
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<ErrorResponse> handleException(final Exception exception) {
+        log.warn("Exception occur: ", exception);
+        return this.makeErrorResponseEntity(MembershipErrorResult.UNKNOWN_EXCEPTION);
+    }
+
+    private ResponseEntity<ErrorResponse> makeErrorResponseEntity(final MembershipErrorResult errorResult) {
+        return ResponseEntity.status(errorResult.getHttpStatus())
+                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class ErrorResponse{
+        private final String code;
+        private final String message;
+    }
+}

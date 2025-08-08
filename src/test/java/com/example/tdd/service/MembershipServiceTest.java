34e9a49d68bb4bc25af8f6126502372104b5b0a8
ldr <hojun9707@naver.com>
2025-08-08 13:21:46 +0900

    test: 서비스계층에서 멤버십등록 및 예외처리 테스트

---
+++ src/test/java/com/example/tdd/service/MembershipServiceTest.java
@@ -0,0 +1,75 @@
+package com.example.tdd.service;
+
+
+import com.example.tdd.dto.MembershipResponse;
+import com.example.tdd.entity.Membership;
+import com.example.tdd.entity.MembershipType;
+import com.example.tdd.exception.MembershipErrorResult;
+import com.example.tdd.exception.MembershipException;
+import com.example.tdd.repository.MembershipRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MembershipServiceTest {
+
+    private final String userId = "userId";
+    private final MembershipType membershipType = MembershipType.NAVER;
+    private final Integer point = 10000;
+
+    @InjectMocks
+    private MembershipService target;
+
+    @Mock
+    private MembershipRepository membershipRepository;
+
+    @Test
+    @DisplayName("멤버십등록실패_이미존재함")
+    public void alreadyMembership() {
+        // given
+        doReturn(Membership.builder().build())
+                .when(membershipRepository).findByUserIdAndMembershipType(userId, membershipType);
+
+        // when
+        final MembershipException result = assertThrows(MembershipException.class, () -> target.addMembership(userId, membershipType, point));
+
+        // then
+        assertThat(result.getErrorResult()).isEqualTo(MembershipErrorResult.DUPLICATED_MEMBERSHIP_REGISTER);
+    }
+
+    @Test
+    @DisplayName("멤버십등록성공")
+    public void membershipRegisterSuccess() {
+        // given
+        doReturn(null).when(membershipRepository).findByUserIdAndMembershipType(userId, membershipType);
+        doReturn(membership()).when(membershipRepository).save(any(Membership.class));
+
+        // when
+        final MembershipResponse result = target.addMembership(userId, membershipType, point);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getMembershipType()).isEqualTo(MembershipType.NAVER);
+
+        // verify
+        verify(membershipRepository, times(1)).findByUserIdAndMembershipType(userId, membershipType);
+        verify(membershipRepository, times(1)).save(any(Membership.class));
+    }
+    private Membership membership() {
+        return Membership.builder()
+                .id(-1L)
+                .userId(userId)
+                .point(point)
+                .membershipType(MembershipType.NAVER)
+                .build();
+    }
+}

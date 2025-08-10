1620c261da491eb626e07515980917c4dbea4191
ho-jun97 <hojun9707@naver.com>
2025-08-10 18:03:22 +0900

    feat: 서비스 계층에서 membership 상세 조회 기능 추가

--- src/main/java/com/example/tdd/service/MembershipService.java
+++ src/main/java/com/example/tdd/service/MembershipService.java
@@ -47,4 +47,15 @@ public class MembershipService {
                 .map(MembershipDetailResponse::of)
                 .toList();
     }
+
+    public MembershipDetailResponse getMembership(final Long membershipId, final String userId) {
+        final Membership membership = membershipRepository.findById(membershipId).orElseThrow(
+                () -> new MembershipException(MembershipErrorResult.MEMBERSHIP_NOT_FOUND));
+
+        if (!membership.getUserId().equals(userId)) {
+            throw new MembershipException(MembershipErrorResult.NOT_MEMBERSHIP_OWNER);
+        }
+
+        return MembershipDetailResponse.of(membership);
+    }
 }

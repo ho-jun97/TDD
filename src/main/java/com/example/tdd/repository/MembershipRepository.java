c7512fb54515a1f3c01506f8b4c80f59f8506a79
ho-jun97 <hojun9707@naver.com>
2025-08-10 16:58:35 +0900

    feat: userId로 전체 조회 기능추가

--- src/main/java/com/example/tdd/repository/MembershipRepository.java
+++ src/main/java/com/example/tdd/repository/MembershipRepository.java
@@ -4,7 +4,11 @@ import com.example.tdd.entity.Membership;
 import com.example.tdd.entity.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // @Respository를 붙어주어야 정상이지만 JpaRepsoitory 하위에 @Repository가 붙어있으므로 붙어주지 않아도 됨
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Membership findByUserIdAndMembershipType(String userId, MembershipType membershipType);
+
+    List<Membership> findAllByUserId(final String userId);
 }

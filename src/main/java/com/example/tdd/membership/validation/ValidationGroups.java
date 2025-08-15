f8f04383795d7f70b929b68365a17a1459f46598
ho-jun97 <hojun9707@naver.com>
2025-08-15 19:43:38 +0900

    feat: 멤버십 포인트 적립

---
+++ src/main/java/com/example/tdd/membership/validation/ValidationGroups.java
@@ -0,0 +1,14 @@
+package com.example.tdd.membership.validation;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ValidationGroups {
+
+    public interface MembershipAddMarker {
+
+    }
+    public interface MembershipAccumulateMarker {
+
+    }
+}
